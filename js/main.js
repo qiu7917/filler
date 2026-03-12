dbfeb02523f120b4c64932eb7cd8df647ebcefe6
qiu7917 <chupa_p@hotmail.com>
2026-03-12 22:37:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    /*=================================================
+    Slick
+    ===================================================*/
+    $(".slider").slick({
+        autoplay: false,
+        infinite: true,
+        arrows: true,
+        prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+        nextArrow: '<div class="slide-arrow next-arrow"></div>',
+        dots: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+    });
+
+
+    /*=================================================
+    アコーディオンメニュー
+    ===================================================*/
+    $('.accordion').on('click', function (e) {
+        $('.accordion').not(this).removeAttr('open');
+    });
+
+
+    /*=================================================
+    fixedbtn
+    ===================================================*/
+
+    $(function () {
+        let cta = $(".fixedbtn");
+        cta.hide();
+
+        $(window).scroll(function () {
+            let scrollThreshold = 500;
+            if ($(window).scrollTop() > scrollThreshold) {
+                cta.fadeIn();
+            } else {
+                cta.fadeOut();
+            }
+        });
+    });
+
+
+    $(window).on("scroll resize", function () {
+        if (window.innerWidth <= 768) {
+            let ctaHeight = $(".fixedbtn").outerHeight();
+            ($(".fixedbtn").is(":visible"))
+            $("footer").css("padding-bottom", ctaHeight + "px");
+
+        } else {
+            $("footer").css("padding-bottom", "100px");
+        }
+    });
+});
